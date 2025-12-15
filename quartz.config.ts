d1633935a275a911db3d49854d527caf61022c62
Ryan Kubik <rmkubik@me.com>
2025-12-14 16:15:23 -0800

    Change baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ryankubik.com",
+    baseUrl: "ryankubik.com/words/raw",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
