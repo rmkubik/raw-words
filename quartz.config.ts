56f257a4e62f2fff0042f6eee8e5afdfb4fe38b3
Ryan Kubik <rmkubik@me.com>
2025-12-16 20:32:27 -0800

    Try notes subdomain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ryankubik.com/words/raw",
+    baseUrl: "notes.ryankubik.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
